f138ffca624ebf9a38dd5429ed2f6cdf243ed570
sampathshetty0303-hub <sampathshetty0303@gmail.com>
2026-08-24 21:59:39 +0530

    Refresh mobile app name

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shuttle-shell-v1";
+const CACHE_NAME = "shuttle-shell-v2";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
